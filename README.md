3da026cd4c4d22b0b64acc557e1f740176d0333b
nfischer <nadav.fischer@capriza.com>
2015-09-06 18:33:57 +0300

    socket gd

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
 # socketgd
-Guaranteed delivery for socket.io
+
+Guaranteed delivery for socket.io. Works in node and in the browser.
+
+## Huh?
+
+socketgd is a wrapper around socket.io sockets that provides reliable message delivery. It tracks messages sent
+over the socket, and waits for `ack`s to be received from the other end indicating that the messages
+were handled successfully.
+
+If a socket gets disconnected then the application can provide a new socket to
+the socketgd wrapper.
+In the client, all messages that have not received an ack from the server will be sent again over the new socket.
+In the server, if messages arrive that it has already acked, then they will be silently discarded.
+
+## Client Usage
+
+```javascript
+var SocketGD = require('socketgd').SocketGD;
+
+// in the browser
+// <script src="socketgd.js"></script>
+
+// create the socket.io socket
+var socket = io(url, config);
+
+// wrap the socket with socketgd
+var socketgd = new SocketGD(socket);
+
+// use socketgd instead of the socket
+socketgd.on('connect', function() {
+  console.log('I'm connected!');
+});
+
+socketgd.send('I am sending you a message');
+socketgd.emit('your-event', {hi: 'there'});
+
+// ...the socket gets disconnected before receiving an ack for 'your-event'...
+
+// the un-acked 'your-event' message will be silently sent again
+socketgd.setSocket(newSocket);
+
+```
+
+## Server Usage
+
+```javascript
+var SocketGD = require('socketgd').SocketGD;
+var socketgd = new SocketGD();
+
+// socket.io server
+io.on('connection', function(socket) {
+
+  // set the underlying socket in socketgd.
+  // if any already acked messages arrive they will be silently discarded
+  socketgd.setSocket(socket);
+
+  socketgd.on('message', function(message, ack, msgId) {
+    console.log('Got message ' + message + ' with id ' + msgId);
+    // finished handling the message, ack it
+    ack();
+  });
+
+  socketgd.on('my-event', function(data, ack, msgId) {
+    console.log('Got my-event ' + data + ' with id ' + msgId);
+    // finished handling the event, ack it
+    ack();
+  });
+
+});
+```
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2015 Capriza Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
---
+++ socketgd.js
@@ -0,0 +1,282 @@
+(function(exporter) {
+
+  /**
+   * socket.io guaranteed delivery socket wrapper.
+   * if the socket gets disconnected at any point, it's up to the application to set a new socket to continue
+   * handling messages.
+   * calling 'setSocket' causes all messages that have not received an ack to be sent again.
+   * @constructor
+   */
+  function SocketGD(socket, lastAcked) {
+    this._pending = [];
+    this._events = {};
+    this._id = 0;
+    this._enabled = true;
+    this._onDisconnectCB = SocketGD.prototype._onDisconnect.bind(this);
+    this._onAckCB = SocketGD.prototype._onAck.bind(this);
+    this.setLastAcked(lastAcked);
+    this.setSocket(socket);
+  }
+
+  /**
+   * set the last message id that an ack was sent for
+   * @param lastAcked
+   */
+  SocketGD.prototype.setLastAcked = function(lastAcked) {
+    this._lastAcked = lastAcked || -1;
+  };
+
+  /**
+   * get the last acked message id
+   */
+  SocketGD.prototype.lastAcked = function() {
+    return this._lastAcked;
+  };
+
+  /**
+   * replace the underlying socket.io socket with a new socket. useful in case of a socket getting
+   * disconnected and a new socket is used to continue with the communications
+   * @param socket
+   */
+  SocketGD.prototype.setSocket = function(socket) {
+
+    this._cleanup();
+    this._socket = socket;
+
+    if (this._socket) {
+      this._socket.on('close', this._onDisconnectCB);
+      this._socket.on('disconnect', this._onDisconnectCB);
+      this._socket.on('reconnect_failed', this._onDisconnectCB);
+      this._socket.on('socketgd_ack', this._onAckCB);
+
+      this._sendPending();
+    }
+  };
+
+  /**
+   * send all pending messages that have not received an ack
+   * @private
+   */
+  SocketGD.prototype._sendPending = function() {
+    var _this = this;
+    // send all pending messages that haven't been acked yet
+    this._pending.forEach(function(message) {
+      _this._sendOnSocket(message);
+    });
+  };
+
+  /**
+   * enable or disable sending message with gd. if disabled, then messages will be sent without guaranteeing delivery
+   * in case of socket disconnection/reconnection.
+   */
+  SocketGD.prototype.enable = function(enabled) {
+    this._enabled = enabled;
+  };
+
+  /**
+   * get the underlying socket
+   */
+  SocketGD.prototype.socket = function() {
+    return this._socket;
+  };
+
+  /**
+   * cleanup socket stuff
+   * @private
+   */
+  SocketGD.prototype._cleanup = function() {
+    if (!this._socket) {
+      return;
+    }
+
+    this._socket.removeListener('close', this._onDisconnectCB);
+    this._socket.removeListener('disconnect', this._onDisconnectCB);
+    this._socket.removeListener('reconnect_failed', this._onDisconnectCB);
+    this._socket.removeListener('socketgd_ack', this._onAckCB);
+  };
+
+  /**
+   * invoked when the socket gets disconnected
+   * @private
+   */
+  SocketGD.prototype._onDisconnect = function() {
+    this._cleanup();
+  };
+
+  /**
+   * invoked when an ack arrives
+   * @param ack
+   * @private
+   */
+  SocketGD.prototype._onAck = function(ack) {
+    // got an ack for a message, remove all messages pending an ack up to (and including) the acked message.
+    while (this._pending.length > 0 && this._pending[0].id <= ack.id) {
+      if (this._pending[0].id === ack.id && this._pending[0].ack) {
+        this._pending[0].ack.call(null, ack.data);
+      }
+      this._pending.shift();
+    }
+  };
+
+  /**
+   * send an ack for a message
+   * @private
+   */
+  SocketGD.prototype._sendAck = function(id, data) {
+    if (!this._socket) {
+      return;
+    }
+
+    this._lastAcked = id;
+    this._socket.emit('socketgd_ack', {id: id, data: data});
+    return this._lastAcked;
+  };
+
+  /**
+   * send a message on the underlying socket.io socket
+   * @param message
+   * @private
+   */
+  SocketGD.prototype._sendOnSocket = function(message) {
+    if (this._enabled && message.id === undefined) {
+      message.id = this._id++;
+      message.gd = true;
+      this._pending.push(message);
+    }
+
+    if (!this._socket) {
+      return;
+    }
+
+    var data;
+    switch (message.type) {
+      case 'send':
+        data = message.gd ? 'socketgd:'+message.id+':'+message.msg : message.msg;
+        this._socket.send(data);
+        break;
+      case 'emit':
+        data = message.gd ? {socketgd: message.id, msg: message.msg} : message.msg;
+        this._socket.emit(message.event, data);
+        break;
+    }
+  };
+
+  /**
+   * send a message with gd. this means that if an ack is not received and a new connection is established (by
+   * calling setSocket), the message will be sent again.
+   * @param message
+   * @param ack
+   */
+  SocketGD.prototype.send = function(message, ack) {
+    this._sendOnSocket({type: 'send', msg: message, ack: ack});
+  };
+
+  /**
+   * emit an event with gd. this means that if an ack is not received and a new connection is established (by
+   * calling setSocket), the event will be emitted again.
+   * @param event
+   * @param message
+   * @param ack
+   */
+  SocketGD.prototype.emit = function(event, message, ack) {
+    this._sendOnSocket({type: 'emit', event: event, msg: message, ack: ack});
+  };
+
+  /**
+   * disconnect the socket
+   */
+  SocketGD.prototype.disconnect = function() {
+    this._socket && this._socket.disconnect();
+  };
+
+  /**
+   * disconnectSync the socket
+   */
+  SocketGD.prototype.disconnectSync = function() {
+    this._socket && this._socket.disconnectSync();
+  };
+
+  /**
+   * close the socket
+   */
+  SocketGD.prototype.close = function() {
+    this._socket && this._socket.close();
+  };
+
+  /**
+   * listen for events on the socket. this replaces calling the 'on' method directly on the socket.io socket.
+   * here we take care of acking messages.
+   * @param event
+   * @param cb
+   */
+  SocketGD.prototype.on = function(event, cb) {
+    this._events[event] = this._events[event] || [];
+
+    var _this = this;
+    var cbData = {
+      cb: cb,
+      wrapped: function(data, ack) {
+        if (data && event === 'message') {
+          // parse the message
+          if (data.indexOf('socketgd:') !== 0) {
+            cb(data);
+            return;
+          }
+          // get the id (skipping the socketgd prefix)
+          var index = data.indexOf(':', 9);
+          if (index === -1) {
+            cb(data);
+            return;
+          }
+
+          var id = parseInt(data.substring(9, index));
+          if (id <= _this._lastAcked) {
+            // discard the message since it was already handled and acked
+            return;
+          }
+
+          var message = data.substring(index + 1);
+          // the callback must call the 'ack' function so we can send an ack for the message
+          cb && cb(message, function(ackData) {
+           return _this._sendAck(id, ackData);
+          }, id);
+        } else if (data && typeof data === 'object' && data.socketgd !== undefined) {
+          if (data.socketgd <= _this._lastAcked) {
+            // discard the message since it was already handled and acked
+            return;
+          }
+          cb && cb(data.msg, function(ackData) {
+            return _this._sendAck(data.socketgd, ackData);
+          }, data.socketgd);
+        } else {
+          cb(data, ack);
+        }
+      }
+    };
+
+    this._events[event].push(cbData);
+
+    this._socket.on(event, cbData.wrapped);
+  };
+
+  /**
+   * remove a previously set callback for the specified event
+   */
+  SocketGD.prototype.off =
+      SocketGD.prototype.removeListener = function(event, cb) {
+        if (!this._events[event]) {
+          return;
+        }
+
+        // find the callback to remove
+        for (var i = 0; i < this._events[event].length; ++i) {
+          if (this._events[event][i].cb === cb) {
+            this._socket && this._socket.removeListener(event, this._events[event][i].wrapped);
+            this._events[event].splice(i, 1);
+          }
+        }
+      };
+
+  exporter.SocketGD = SocketGD;
+
+})(typeof module !== 'undefined' && typeof module.exports === 'object' ? module.exports : window);
